c829585a1068086cf96fbd71d9ec0f1af68e1ee6
GLaDOS <gladosconn@gmail.com>
2017-11-01 17:09:59 +0800

    Complete main function.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,9 @@
-#include <iostream>
 #include <boost/program_options.hpp>
+#include <iostream>
 
 #include "args.h"
-#include "ecdsa_key.h"
+#include "base58.h"
+#include "ecdsa_key_man.h"
 
 /**
  * Show help document.
@@ -10,23 +11,28 @@
  * @param args The argument manager
  */
 void ShowHelp(const Args &args) {
-    std::cout << "BTCAddrGen version 0.1" << std::endl;
-    std::cout << "Usage:" << std::endl;
-    std::cout << "  ./btcaddrgen" << std::endl << std::endl;
-    std::cout << "Arguments:" << std::endl;
-    std::cout << args.GetArgsHelpString() << std::endl;
+  std::cout << "BTCAddrGen version 0.1" << std::endl;
+  std::cout << "Usage:" << std::endl;
+  std::cout << "  ./btcaddrgen" << std::endl << std::endl;
+  std::cout << "Arguments:" << std::endl;
+  std::cout << args.GetArgsHelpString() << std::endl;
 }
 
 /// Main program.
 int main(int argc, const char *argv[]) {
-    Args args(argc, argv);
-    if (args.is_help()) {
-        ShowHelp(args);
-        return 0;
-    }
-
-    // Trying to generate a new key pair.
-    ECDSAKeyGenerator generator;
-    std::cout << "Private key: " << generator.get_private_key() << std::endl;
+  Args args(argc, argv);
+  if (args.is_help()) {
+    ShowHelp(args);
     return 0;
+  }
+
+  // Trying to generate a new key pair.
+  auto &man = ecdsa::KeyManager::get_instance();
+  auto key = man.NewKey();
+  std::cout << "public key: " << base58::EncodeBase58(key.get_pub_key())
+            << std::endl;
+  std::cout << "private key: " << base58::EncodeBase58(key.get_priv_key())
+            << std::endl;
+
+  return 0;
 }
