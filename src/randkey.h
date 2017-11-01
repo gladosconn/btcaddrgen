20c4f60359321af72ddc433e5d0ac1b4dd19c368
GLaDOS <gladosconn@gmail.com>
2017-11-01 13:31:37 +0800

    Add randkey.

---
+++ src/randkey.h
@@ -0,0 +1,18 @@
+#ifndef __RAND_KEY_H__
+#define __RAND_KEY_H__
+
+namespace rnd {
+
+/**
+ * Build randomized buffer
+ *
+ * @param out Allocated buffer to save output buffer.
+ * @param num Size of the buffer, must <= 32.
+ *
+ * @return none
+ */
+void GetStrongRandBytes(unsigned char *out, int num);
+
+} // namespace rnd
+
+#endif
