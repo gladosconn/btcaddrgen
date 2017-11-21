6f6926c373cf829cb8043f1a81595c0388d8009f
Matthew Lee <mattxlee@gmail.com>
2017-11-22 00:46:18 +0800

    Add comment for WIF functions.

--- src/btcwif.h
+++ src/btcwif.h
@@ -7,8 +7,22 @@
 namespace btc {
 namespace wif {
 
+/**
+ * Convert private key to string in WIF
+ *
+ * @param priv_key Private key data.
+ *
+ * @return WIF string.
+ */
 std::string PrivateKeyToWif(const std::vector<uint8_t> &priv_key);
 
+/**
+ * Parse WIF private key string.
+ *
+ * @param priv_key_str Private key string in WIF format.
+ *
+ * @return Private key data.
+ */
 std::vector<uint8_t> WifToPrivateKey(const std::string &priv_key_str);
 
 }  // namespace wif
