24f20ef7835b0cd9bf979cd1143cd87b8f5dac30
Matthew Lee <mattxlee@gmail.com>
2017-11-22 00:59:39 +0800

    Add `VerifyWifString` function.

--- src/btcwif.cc
+++ src/btcwif.cc
@@ -56,5 +56,21 @@ std::vector<uint8_t> WifToPrivateKey(const std::string &priv_key_str) {
   return pk2;
 }
 
+bool VerifyWifString(const std::string &priv_key_str) {
+  std::vector<uint8_t> priv_key_data;
+  assert(base58::DecodeBase58(priv_key_str, priv_key_data));
+
+  std::vector<uint8_t> last_4_bytes(4);
+  memcpy(last_4_bytes.data(), priv_key_data.data() + (priv_key_data.size() - 4), 4);
+
+  std::vector<uint8_t> priv_key_data_to_hash(priv_key_data.size() - 4);
+  memcpy(priv_key_data_to_hash.data(), priv_key_data.data(), priv_key_data.size());
+
+  std::vector<uint8_t> check_sum_half = utils::sha256(priv_key_data_to_hash.data(), priv_key_data_to_hash.size());
+  std::vector<uint8_t> check_sum = utils::sha256(check_sum_half.data(), check_sum_half.size());
+
+  return memcmp(check_sum.data(), last_4_bytes.data(), 4) == 0;
+}
+
 }  // namespace wif
 }  // namespace btc
--- src/btcwif.h
+++ src/btcwif.h
@@ -25,6 +25,15 @@ std::string PrivateKeyToWif(const std::vector<uint8_t> &priv_key);
  */
 std::vector<uint8_t> WifToPrivateKey(const std::string &priv_key_str);
 
+/**
+ * Verifying a private key string in WIF format.
+ *
+ * @param priv_key_str Private key string in WIF format.
+ *
+ * @return Returns true if the private key string is in WIF format and valid.
+ */
+bool VerifyWifString(const std::string &priv_key_str);
+
 }  // namespace wif
 }  // namespace btc
 
